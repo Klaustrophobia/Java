3b50a7c77a43893e88cbf98710cf1e32f212d2d4
Klaustrophobia <70920346+Klaustrophobia@users.noreply.github.com>
2023-03-20 10:55:19 -0600

    Add files via upload

---
+++ DOCUMENTACION/Codigos Base/Ejercicio1.java
@@ -0,0 +1,27 @@
+/*
+    Package NombreDelProyecto
+
+    La funcionalidad de una aplicación Java se implementa habitualmente en múltiples clases, 
+    entre las que suelen existir distintas relaciones.  Las clases se agrupan en unidades de un nivel superior, los paquetes, 
+    que actúan como ámbitos de contención de tipos. Cada módulo de código establece, mediante la palabra clave package al inicio, 
+    a qué paquete pertenece. 
+
+
+ */
+public class Ejercicio1{
+
+    public static void main (String[] args){
+        int numero = 100;
+        char caracter = 2;
+        boolean VariableBooleana = false;
+        double decimal = 16;
+        float flotante = 18;
+        
+        System.out.print("Hello");
+        System.out.println(numero);
+        System.out.println(caracter);
+        System.out.println(VariableBooleana);
+        System.out.println(decimal);
+        System.out.println(flotante);
+    }
+}
